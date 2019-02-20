b07ac2a897ce5caf93d7ac36bbe723cbb7edfcec
Bimalkeeth <bkaluarachchi@assetic.com>
2019-02-20 16:00:25 +1100

    new change

--- PowerBIService/Services/Implementation/PowerService.cs
+++ PowerBIService/Services/Implementation/PowerService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Bifrost.Extensions;
-using ClientCommon;
 using ClientCommon.Contract;
 using ClientCommon.Resources;
 using Microsoft.PowerBI.Api.V2;
